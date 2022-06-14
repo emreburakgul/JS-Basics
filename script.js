9b09f8f687d9e1f6843d49abe006fdc0813ff125
Emre Burak GÜL <emreburakgul@gmail.com>
2022-06-15 02:10:44 +0300

    JS-Basics

---
+++ script.js
@@ -0,0 +1,24 @@
+let squareDiv =document.getElementById('square')
+
+/*
+    JavaScript dilinin aslında Java ile ilgisi yok
+    JS büyük-küçük harf duyarlı bir dildir.
+    JS esnek bir dildir (Loosely Typed)
+    JS'te en yaygın kullanılan veri tipleri
+        -Strng,boolean,number,Object(array),Undefined
+    
+    JS kodlarını nerede çağırmam/yazmam gerektiğinin biliyorum.
+    JS kodlarını hangi anda çalıştığını biliyorum.
+
+    DOM -Document Object Model
+    JavaScript demek document.getElementById demek değildir. bunu biliyorum 
+    DOM: Metin  bazlı bir dosya olan HTML belgesinin nesne tabanlı bir kod-kütüphanesi
+    olduğunu biliorum
+
+    Metotlar
+        getElementById
+        getElementById/ByTagName/ByClassName
+    HTML kodlamasında attribute oalrak kullandığımız id,style,class,name gibi
+    özelleikleriden DOM içerisinde bir nesne propertysi gibi erişildiğini biliyorum
+
+*/ 
